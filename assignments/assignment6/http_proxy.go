00e2b05369b9dad013c4ce93f6ad1faffdadbe52
petermwesigwa <mps1401@gmail.com>
2020-05-08 22:26:56 -0400

    somewhat close

--- assignments/assignment6/http_proxy.go
+++ assignments/assignment6/http_proxy.go
@@ -1,89 +1,118 @@
 /*****************************************************************************
- * http_proxy.go                                                                 
- * Names: 
+ * http_proxy.go
+ * Names:
  * NetIds:
  *****************************************************************************/
 
- // TODO: implement an HTTP proxy
+// TODO: implement an HTTP proxy
 
- package main
+package main
 
- import (
- 	"os"
- 	"net"
- 	"net/http"
- 	"bufio"
- 	"log"
- 	//"io"
- 	//"fmt",
- 	//"bytes"
- )
+import (
+	"bufio"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	//"io"
+	//"fmt",
+	//"bytes"
+)
 
- func main() {
- 	var port string
- 	if len(os.Args) < 2 {
-    	port = "80"
-  	} else {
-  		port = os.Args[1]
-  	}
+func main() {
+	var port string
+	if len(os.Args) < 2 {
+		port = "80"
+	} else {
+		port = os.Args[1]
+	}
 
-  	// listen for connection
-	ln, err := net.Listen("tcp", ":" + port)
-  	if err != nil {
-    	log.Fatal(err)
-  	}
+	// listen for connection
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  	for {
-  		// accept a connection
-  		conn, err := ln.Accept()
-    	if err != nil {
-    		continue
-    	}
+	for {
+		// accept a connection
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
 
-  		// spawn go routine to handle connection
-  		go handle_http(conn)
-  	}
+		// spawn go routine to handle connection
+		go handle_http(conn)
+	}
 
+}
 
- }
+func handle_http(conn net.Conn) {
 
- func handle_http(conn net.Conn) {
+	BUFFER_SIZE := 100000
 
- 	BUFFER_SIZE := 100000
+	// read data into buffer
+	rb := make([]byte, BUFFER_SIZE)
+	wb := make([]byte, BUFFER_SIZE)
+	_, err := conn.Read(rb)
 
- 	// read data into buffer
-  	rb := make([]byte, BUFFER_SIZE)
-  	wb := make([]byte, BUFFER_SIZE)
- 	n, err := conn.Read(rb)
+	// write request string to a file
+	rfd, err := os.Create("/tmp/read")
+	if err != nil {
+		resp := []byte("HTTP/1.1 500 Internal Server Error\r\n ")
+		conn.Write(resp)
+		conn.Close()
+		return
+	}
 
- 	// create bufio reader
- 	r := bufio.NewReader(rb)
+	wfd, err := os.Create("/tmp/write")
+	if err != nil {
+		resp := []byte("HTTP/1.1 500 Internal Server Error\r\n Failed to create write file\r\n")
+		conn.Write(resp)
+		conn.Close()
+		return
+	}
+	rfd.Write(rb)
 
- 	// read data into request data type
- 	req, err := http.ReadRequest(r)
- 	if err != nil || req.Method != "GET" {
- 		resp := []byte("HTTP/1.1 500 Internal Server Error\r\n")
- 		conn.Write(resp)
- 		conn.Close()
- 		return
- 	}
+	// create bufio reader
+	r := bufio.NewReader(rfd)
 
- 	// add connection = close
- 	req.Header.Del("Connection")
- 	req.Header.Add("Connection", "close")
+	// lines := strings.Split(string(rb), "\r\n")
+	// params := strings.Split(lines[0], " ")
+	// if params[0] != "GET" || params[2] != "HTTP/1.1" {
+	// 	resp := []byte("HTTP/1.1 500 Internal Server Error\r\n")
+	// 	conn.Write(resp)
+	// 	conn.Close()
+	// 	return
+	// }
 
- 	// send request
- 	client := &http.Client{}
+	// read data into request data type
+	req, _ := http.ReadRequest(r)
+	if req.Method != "GET" {
+		resp := []byte("HTTP/1.1 500 Internal Server Error\r\n")
+		log.Panic(err.Error())
+		conn.Write(resp)
+		conn.Close()
+		return
+	}
+
+	// add connection = close
+	req.Header.Del("Connection")
+	req.Header.Add("Connection", "close")
+
+	// send request
+	client := &http.Client{}
 	resp, err := client.Do(req)
 
 	// write to buffer and then to connection
-	err = resp.Write(wb)
+	err = resp.Write(wfd)
+	wfd.Read(wb)
 	conn.Write(wb)
 
 	// close connection
 	conn.Close()
 
- }
+}
+
 /*
  func client(server_ip string, server_port string) {
 
@@ -96,7 +125,7 @@
   }
 
   r := bufio.NewReader(os.Stdin)
-  
+
   n, err := r.Read(b)
   for n == SEND_BUFFER_SIZE {
     conn.Write(b[:n])
@@ -105,7 +134,7 @@
   conn.Write(b[:n])
 
 }
- 
+
 
 func server(server_port string) {
 
@@ -131,10 +160,9 @@ func server(server_port string) {
     //fmt.Printf(string(b[:n]))
     os.Stdout.Write(b[:n])
 
-    
+
   }
 
 }
 
 */
-
